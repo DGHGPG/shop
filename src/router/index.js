1c29879a562779c6ec011350d91f2d8b557523b8
DGHGPG <595395562@qq.com>
2018-01-07 16:14:46 +0800

    GoodsList组件拆分，修正了HelloWorld组件

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 import GoodsList from '@/views/GoodsList'
 import Cart from '@/views/Cart'
 Vue.use(Router)
@@ -9,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'home',
+      component: GoodsList
     },
     {
       path: '/GoodsList',
---
+++ src/views/Cart.vue
@@ -0,0 +1,24 @@
+/**
+* Created by DGHGPG on 2017/12/22
+*/
+<template>
+  <div class="cart">
+    {{ message }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'cart',
+  data () {
+    return {
+      message: 'cart'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
